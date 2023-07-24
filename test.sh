bd5cf0c3acd2694f2447d372eeeab648f7a73890
FreddyWordingham <freddy@digilab.co.uk>
2023-07-24 20:05:31 +0100

    Added success command

--- test.sh
+++ test.sh
@@ -11,22 +11,23 @@ wait_time=1
 
 # Run tests
 get_user &&
-get_versions &&
-upload_dataset $dataset_path $dataset_name &&
-list_datasets &&
-view_dataset $dataset_name &&
-summarise_dataset $dataset_name &&
-list_models &&
-train_model $train_path $model_name $processor &&
-job_complete=false
+    get_versions &&
+    upload_dataset $dataset_path $dataset_name &&
+    list_datasets &&
+    view_dataset $dataset_name &&
+    summarise_dataset $dataset_name &&
+    list_models &&
+    train_model $train_path $model_name $processor &&
+    job_complete=false
 while [ "$job_complete" = false ]; do
     response=$(status_model $model_name)
     job_complete=$(echo $response | jq -r '.job_complete')
     echo "Job finished: $job_complete"
     sleep $wait_time
 done &&
-list_models &&
-summarise_model $model_name &&
-use_model $predict_path $model_name "predict" $processor &&
-delete_model $model_name &&
-delete_dataset $dataset_name
+    list_models &&
+    summarise_model $model_name &&
+    use_model $predict_path $model_name "predict" $processor &&
+    delete_model $model_name &&
+    delete_dataset $dataset_name &&
+    echo "All tests passed!"
